a34271fc525c1a9e8dedd28daad7e2464806742a
Kazuhiro Osawa <yappo@shibuya.pl>
2012-08-31 12:58:48 +0900

    initial commit

---
+++ lib/Plack/Middleware/DaisukeLog.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::DaisukeLog;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::DaisukeLog -
+
+=head1 SYNOPSIS
+
+    use Plack::Middleware::DaisukeLog;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::DaisukeLog is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
